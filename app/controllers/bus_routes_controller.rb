846cb6858d1f3633b516b38f2d278db60984704a
Erinna Chen <erinna.chen@gmail.com>
2017-10-01 18:02:53 -0500

    Remove search route

--- app/controllers/bus_routes_controller.rb
+++ app/controllers/bus_routes_controller.rb
@@ -2,13 +2,4 @@ class BusRoutesController < ApplicationController
   def show
     @bus_route = BusRoute.find(params.require(:id))
   end
-
-  def search
-    @bus_route = BusRoute.find_by!(external_id: search_params[:external_id])
-  end
-
-  private
-  def search_params
-    params.require(:bus_route).permit(:external_id)
-  end
 end
