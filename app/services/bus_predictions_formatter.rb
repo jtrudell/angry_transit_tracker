7c0d41373134742a3a0f39d22f1f4361a8eb3670
Jen <jtrudell@gmail.com>
2017-10-06 17:37:04 -0500

    better handling of predictions hash

--- app/services/bus_predictions_formatter.rb
+++ app/services/bus_predictions_formatter.rb
@@ -1,10 +1,10 @@
 class BusPredictionsFormatter
-  attr_reader :predictions, :format_predictions, :errors
+  attr_reader :predictions, :formatted_predictions, :errors
 
   def initialize(raw_predictions)
     @predictions = raw_predictions
-    @formatted_predictions = nil
-    @errors = nil
+    @formatted_predictions = {}
+    @errors = {}
   end
 
   def execute
@@ -12,29 +12,42 @@ class BusPredictionsFormatter
     return errors if errors.present?
 
     format_predictions
+    formatted_predictions
   end
 
   private
 
   def error_check
-    if predictions['error']
-      @errors = predictions['error'].first
-      @errors['stop_number'] = @errors.delete('stpid')
-      @errors['route'] = @errors.delete('rt')
-      @errors['message'] = @errors.delete('msg')
+    if prediction_error
+      @errors['stop_number'] = prediction_error.fetch('stpid', nil)
+      @errors['route']       = prediction_error.fetch('rt', nil)
+      @errors['message']     = prediction_error.fetch('msg', nil)
     end
   end
 
+  def prediction
+    predictions['prd']&.first
+  end
+
+  def prediction_error
+    predictions['error']&.first
+  end
+
   def format_predictions
-    prediction = predictions['prd'].first
-    formatted_prediction = {}
-    formatted_prediction['route'] = prediction['rt']
-    formatted_prediction['stop_number'] = prediction['stpid']
-    formatted_prediction['minutes'] = prediction['prdctdn']
-    formatted_prediction['arrival_time'] = prediction['prdtm'].to_datetime.strftime("%I:%M%p on %m/%d/%Y")
-    formatted_prediction['delayed'] = prediction['dly']
-    formatted_prediction['distance'] = prediction['dstp'].to_s + " feet away"
-    formatted_prediction['vehicle_id'] = prediction['vid']
-    formatted_prediction
+    @formatted_predictions['route']        = prediction.fetch('rt', nil)
+    @formatted_predictions['stop_number']  = prediction.fetch('stpid', nil)
+    @formatted_predictions['minutes']      = prediction.fetch('prdctdn', nil)
+    @formatted_predictions['arrival_time'] = formatted_arrival_time
+    @formatted_predictions['delayed']      = prediction.fetch('dly', nil)
+    @formatted_predictions['distance']     = formatted_distance
+    @formatted_predictions['vehicle_id']   = prediction.fetch('vid', nil)
+  end
+
+  def formatted_arrival_time
+    prediction.fetch('prdtm', nil)&.to_datetime.strftime("%I:%M%p on %m/%d/%Y")
+  end
+
+  def formatted_distance
+    prediction.fetch('dstp', nil)&.to_s + " feet away"
   end
 end
