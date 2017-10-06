e967ea67efe56f3ad50732ef4fbba0a296ce8866
Jen <jtrudell@gmail.com>
2017-10-06 11:44:44 -0500

    test serializers

---
+++ test/serializers/bus_stop_serializer_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class BusStopSerializerTest < ActiveSupport::TestCase
+  test "serializer" do
+    bus_stop = BusStop.new(stop_id: '555', name: 'My Awesome Bus Stop', direction: 'Northbound')
+    serializer = BusStopSerializer.new(bus_stop)
+
+    expected = {
+      "bus_stop"=> {
+         :id=>nil,
+         :stop_id=>"555",
+         :name=>"My Awesome Bus Stop",
+         :direction=>"Northbound",
+         :display_name=>"555: My Awesome Bus Stop (Northbound)",
+         :created_at=>nil,
+         :updated_at=>nil,
+         :bus_routes=>[]
+      }
+    }
+
+    assert_equal expected.to_json, serializer.to_json
+  end
+end
